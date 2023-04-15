199e5a83b10dfbf68bf0d938d24d0417643890eb
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-04-15 15:36:01 +0530

    switched to printf func for formatted data

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ func main() { //entry point
 	const conferenceTickets = 50 //cant be changed
 	var remainingTickets = 50    //decreases as tickets get booked.
 
-	fmt.Println("Welcome to", conferenceName, "booking application") //Println ends the output with a new line.
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are remaining.")
+	// Println ends the output with a new line.
+	// Printf is for formatted data
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are remaining.\n", conferenceTickets, remainingTickets)
 	fmt.Print("Get your tickets here to attend.")
 	fmt.Println("")
 }

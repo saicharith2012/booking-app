7a12ed8cea5243108d439b606298722fba75f259
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-04-15 16:35:08 +0530

    adding type declaration and variables for users

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import "fmt" //Format Package
 
 func main() { //entry point
 
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50 //cant be changed
-	var remainingTickets = 50    //decreases as tickets get booked.
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50 //cant be changed
+	var remainingTickets int = 50    //decreases as tickets get booked.
+
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 
 	// Println ends the output with a new line.
 	// Printf is for formatted data
@@ -14,4 +16,13 @@ func main() { //entry point
 	fmt.Printf("We have a total of %v tickets and %v are remaining.\n", conferenceTickets, remainingTickets)
 	fmt.Print("Get your tickets here to attend.")
 	fmt.Println("")
+
+	var userName string
+	var userTickets int
+	// ask the user for name and no.of tickets
+
+	userName = "Dameon"
+	userTickets = 3
+	fmt.Printf("%v has booked %v tickets.\n", userName, userTickets)
+
 }

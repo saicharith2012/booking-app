62a7e5e30c66a6df6e532b3c4aeb5c50d240e6cb
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-04-19 23:02:19 +0530

    exported validUserInputs func to another go file

---
+++ helper.go
@@ -0,0 +1,12 @@
+package main
+
+import "strings"
+
+func validateUserInputs(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2 // firstName and lastName should contain atleast two letters.
+	isValidEmail := strings.Contains(email, "@")             // email must contain @ symbol
+	isTicketNumberPositive := userTickets > 0
+	isValidTicketNumber := userTickets <= remainingTickets // userTickets must be positive and less or equal to remaining tickets.
+
+	return isValidName, isValidEmail, isTicketNumberPositive, isValidTicketNumber
+}
--- main.go
+++ main.go
@@ -97,15 +97,6 @@ func getFirstNames([]string) []string {
 	return firstNames
 }
 
-func validateUserInputs(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2 // firstName and lastName should contain atleast two letters.
-	isValidEmail := strings.Contains(email, "@")             // email must contain @ symbol
-	isTicketNumberPositive := userTickets > 0
-	isValidTicketNumber := userTickets <= remainingTickets // userTickets must be positive and less or equal to remaining tickets.
-
-	return isValidName, isValidEmail, isTicketNumberPositive, isValidTicketNumber
-}
-
 func getUserInputs() (string, string, string, uint) {
 	var firstName string
 	var lastName string

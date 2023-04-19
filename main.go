e7fef956762b217cd7a83a4cce3aa27628754978
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-04-19 17:18:07 +0530

    added few package level variables and removed redudant arguments

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"strings"
 )
 
-func main() { //entry point
+// package level variables
+// variable cant be declared with short declaration at package levels.
+var conferenceName = "Go Conference" // short declaration...const cannot be declared like this and type declaration cannot be done explicitly
+const conferenceTickets uint = 50    //cant be changed
+var remainingTickets uint = 50       //decreases as tickets get booked.
+var bookings []string
 
-	conferenceName := "Go Conference" // short declaration...const cannot be declared like this and type declaration cannot be done explicitly
-	const conferenceTickets uint = 50 //cant be changed
-	var remainingTickets uint = 50    //decreases as tickets get booked.
-	var bookings []string
+func main() { //entry point
 
 	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 
@@ -20,19 +22,19 @@ func main() { //entry point
 	//adding an Infinite For loop to execute the code of bookings multiple times.
 	for {
 
-		greetUsers(conferenceName, conferenceTickets, remainingTickets)
+		greetUsers()
 
 		// asking user for inputs
 		firstName, lastName, email, userTickets := getUserInputs()
 
 		// user input validation
 		isValidName, isValidEmail, isTicketNumberPositive, isValidTicketNumber :=
-			validateUserInputs(firstName, lastName, email, userTickets, remainingTickets)
+			validateUserInputs(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isTicketNumberPositive && isValidTicketNumber {
 
 			// calling bookTickets
-			bookings, remainingTickets = bookTickets(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
+			bookTickets(userTickets, firstName, lastName, email)
 
 			// calling the function for printing the first names
 			var firstNames = getFirstNames(bookings) //return value of getFirstNames
@@ -80,13 +82,13 @@ func main() { //entry point
 	// }
 }
 
-func greetUsers(confName string, confTickets uint, remTickets uint) {
-	fmt.Printf("Welcome to %v booking application\n", confName)
-	fmt.Printf("We have a total of %v tickets and %v are remaining.\n", confTickets, remTickets)
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are remaining.\n", conferenceTickets, remainingTickets)
 	fmt.Print("Get your tickets here to attend.\n")
 }
 
-func getFirstNames(bookings []string) []string {
+func getFirstNames([]string) []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
 		var names = strings.Fields(booking)
@@ -95,7 +97,7 @@ func getFirstNames(bookings []string) []string {
 	return firstNames
 }
 
-func validateUserInputs(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool, bool) {
+func validateUserInputs(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2 // firstName and lastName should contain atleast two letters.
 	isValidEmail := strings.Contains(email, "@")             // email must contain @ symbol
 	isTicketNumberPositive := userTickets > 0
@@ -127,7 +129,7 @@ func getUserInputs() (string, string, string, uint) {
 
 }
 
-func bookTickets(remainingTickets uint, userTickets uint, bookings []string, firstName string, lastName string, email string, conferenceName string) ([]string, uint) {
+func bookTickets(userTickets uint, firstName string, lastName string, email string) ([]string, uint) {
 	remainingTickets = remainingTickets - userTickets //remaining tickets after booking.
 	bookings = append(bookings, firstName+" "+lastName+",")
 
